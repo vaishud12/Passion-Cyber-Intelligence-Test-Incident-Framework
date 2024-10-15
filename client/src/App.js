97af536b0ac5e3d2056ca33f677736082068ae2c
Vaishnavi <vaishuu2397@gmail.com>
2024-10-15 12:59:09 +0530

    route

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 // App.js
 import React from 'react';
+import PrivateRoute from './components/PrivateRoute';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Home'; // This is already wrapped
 import Signup from './Signup-Login/Signup'; // This is already wrapped
@@ -7,7 +8,7 @@ import Login from './Signup-Login/Login'; // Wrap this similarly
 import ForgetPassword from './Signup-Login/ForgetPassword'; // Wrap this similarly
 import ResetPassword from './Signup-Login/ResetPassword'; // Wrap this similarly
 import AdminMain from './BotAnalysis/AdminMain'; // Wrap this similarly
-import Chatbot from './BotAnalysis/Chatbot'; // Wrap this similarly
+
 import './App.css';
 
 const App = () => {
@@ -18,9 +19,9 @@ const App = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Home />} />
         <Route path="/forgetPassword" element={<ForgetPassword />} />
-        <Route path="/reset-password/:token" element={<ResetPassword />} />
-        <Route path="/Adminmain" element={<AdminMain />} />
-        <Route path="/chatbot" element={<Chatbot />} />
+        <Route path="/reset-password/:token" element={<PrivateRoute><ResetPassword /></PrivateRoute>} />
+        <Route path="/Adminmain" element={<PrivateRoute><AdminMain /></PrivateRoute>} />
+        
       </Routes>
     </Router>
   );
